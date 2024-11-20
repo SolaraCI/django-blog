b6ba4ea7edb14da9fce8bd68f7cf4100efbe0c15
Jonathan Coton <fbp3sgxt@students.codeinstitute.net>
2024-11-20 14:09:16 +0000

    migrated model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = {
     'default': os.environ.get("SECRET_KEY")
 }
-# SECRET_KEY = "django-erucesnui-d4x_qey*!vorrmb+_23!=v^yy)%cn6)qtgoh4)2&yh$x&0f6x+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,6 +91,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
